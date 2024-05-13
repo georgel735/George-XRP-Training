cedd7d9dab2c57fb73e8be1b7f6ff43256ff7f77
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2024-05-13 15:07:31 -0400

    added N sided shape command

--- src/main/java/frc/robot/commands/shapes/NSidedShapeCommand.java
+++ src/main/java/frc/robot/commands/shapes/NSidedShapeCommand.java
@@ -4,14 +4,16 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.XRPDrivetrain;
 
 //TODO: This command should have the drivetrain move in a triangle fashion
-public class TriangleCommand extends Command {
+public class NSidedShapeCommand extends Command {
   private final XRPDrivetrain m_drivetrain;
+  private final double m_sides;
 
   // use this boolean to tell the command when it should finish
   private boolean isFinished = false;
 
-  public TriangleCommand(XRPDrivetrain drivetrain) {
+  public NSidedShapeCommand(XRPDrivetrain drivetrain, double sides) {
     m_drivetrain = drivetrain;
+    m_sides = sides;
     // each subsystem used by the command must be passed into the
     // addRequirements() method (which takes a vararg of Subsystem)
     addRequirements(m_drivetrain);
@@ -19,13 +21,13 @@ public class TriangleCommand extends Command {
 
   @Override
   public void execute() {
-    // Repeat the same motions 3 times (for a triangle)
-    for (int i = 0; i < 3; i++) {
+    // Repeat the same motion once for each side
+    for (int i = 0; i < m_sides; i++) {
       // drive at half speed for 1 second
       m_drivetrain.driveForTime(0.5, 0.0, 1);
 
       // turn 120 degrees (360 / 3 = 120)
-      m_drivetrain.rotateDegrees(120);
+      m_drivetrain.rotateDegrees(360.0 / m_sides);
     }
 
     // Stop command
